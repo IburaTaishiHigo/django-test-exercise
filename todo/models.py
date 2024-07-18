094bda128c656a84a2560c8ce08b25ac8daeff4a
1f10230216 <s1f102302168@iniad.org>
2024-07-18 12:34:26 +0900

    優先度設定の追加

--- todo/models.py
+++ todo/models.py
@@ -4,10 +4,17 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', '低'),
+        ('medium', '中'),
+        ('high', '高'),
+    ]
+    
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='medium')
 
     def is_overdue(self, dt):
         if self.due_at is None:
--- todo/views.py
+++ todo/views.py
@@ -9,7 +9,8 @@ from todo.models import Task
 def index(request):
     if request.method == "POST":
         task = Task(title=request.POST["title"],
-                    due_at=make_aware(parse_datetime(request.POST["due_at"])))
+                    due_at=make_aware(parse_datetime(request.POST["due_at"])),
+                    priority=request.POST["priority"])
         task.save()
 
     if request.GET.get("order") == "due":
